4e13f79b3c6870d9587a96d8cd5883cfc9ac9de7
Shelley <you@example.com>
2016-07-28 21:12:19 -0700

    finishing up

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
+var query = '';
+var baseURL = 'https://en.wikipedia.org/wiki/'
 
+function emptyInput() {
+  $('#searchResults').empty();
+}
+
+$('#getIt').hide();
+
+$('#searchText').click(function () {
+  $('#getIt').show();
+});
+
+$('#searchText').keyup(function (e) {
+  if (e.which == 13) {
+    $('#getIt').click();
+    e.preventDefault();
+  }
+});
+
+$('#getIt').click(function () {
+  emptyInput();
+  $('#results').removeClass('results').addClass('animated slideInUp');
+  query = $('#searchText').val();
+  var queryURL =
+    'https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=' +
+    query + '&callback=?'
+
+  $.getJSON(queryURL, function (data) {
+    for (var n = 0; n < 10; n++) {
+      var term = data.query.search[n].title,
+        snip = data.query.search[n].snippet,
+        titleLink = '<a class="title" href="' + baseURL + term +
+        '" target="_blank">' +
+        term + '</a>';
+      $('#searchResults').append('<dt>' +
+        titleLink + '</dt>');
+      $('#searchResults').append('<dd class="snip">' + snip +
+        ' &hellip;' + '</dd>');
+    }
+  });
+})
